122b7704a9ae202aca7d6fca71b97f3b5d2d7bfd
abisop <abhishekchoithani24@gmail.com>
2024-09-27 01:31:12 +0530

    removing build files

--- nuttx_ws/b/modules/pid/CMakeFiles/nuttx.dir/main.cpp.o.d
+++
@@ -1,2 +0,0 @@
-modules/pid/CMakeFiles/nuttx.dir/main.cpp.o: \
- /home/abisop-nvme/project_debugger/togit/app/modules/pid/main.cpp
--- nuttx_ws/b/modules/pid/CMakeFiles/nuttx.dir/pid.c.o.d
+++
@@ -1,2 +0,0 @@
-modules/pid/CMakeFiles/nuttx.dir/pid.c.o: \
- /home/abisop-nvme/project_debugger/togit/app/modules/pid/pid.c
